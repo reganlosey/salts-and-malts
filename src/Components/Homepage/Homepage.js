77da15b9d039b5e0f4566f6404a5f624c61d7304
Regan Losey <regan.losey@gmail.com>
2022-01-14 16:44:07 -0700

    Add basic sass styling

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,30 @@
+import React, {useEffect,useState } from 'react';
+import './Homepage.scss'
+
+
+
+
+const Homepage = () => {
+  return (
+    <h1>Homepage!</h1>
+  )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Homepage
