5821cd91b364690567ae737e433aaef6be447d56
Regan Losey <regan.losey@gmail.com>
2022-01-16 09:40:02 -0700

    Write tests to assert cards contain links to beer details

--- cypress/integration/Homepage_spec.js
+++ cypress/integration/Homepage_spec.js
@@ -23,13 +23,21 @@ describe('Homepage', () => {
   it('should display cards on a grid that contain beer details', () => {
     cy.get('.homepage-grid')
     .get('.homepage-grid > :nth-child(1)')
-    .get('.homepage-grid > :nth-child(1) > :nth-child(1)')
+    .get(':nth-child(1) > :nth-child(1)')
     .contains('Buzz')
-    .get('.homepage-grid > :nth-child(1) > :nth-child(2)')
+    .get(':nth-child(1) > :nth-child(2)')
     .contains('abv: 4.5')
-    .get('.homepage-grid > :nth-child(1) > :nth-child(3)')
+    .get(':nth-child(1) > :nth-child(3)')
     .contains('ibu: 60')
-    
+    .get(':nth-child(1) > .info-link')
+    .contains('click here to see more')
+  })
+
+  it('should have a link to the specific beer\'s details in each card', () => {
+    cy.get('.homepage-grid')
+    .get('.homepage-grid > :nth-child(1)')
+    .get(':nth-child(1) > .info-link')
+    .should('have.attr', 'href', '/1')
   })
 
 
