43b042134632abcc4460d8ae2cce4c379f26ab14
Regan Losey <regan.losey@gmail.com>
2022-01-15 20:21:30 -0700

    Write tests to assert that cards contain details about each beer

--- cypress/integration/Homepage_spec.js
+++ cypress/integration/Homepage_spec.js
@@ -1,15 +1,15 @@
 describe('Homepage', () => {
   beforeEach(() => {
-    cy.intercept('https://api.punkapi.com/v2/beers', {status: 200, fixture: 'beers'})
-    cy.visit('http://localhost:3000/')
+    cy.intercept('https://api.punkapi.com/v2/beers', {status: 200, fixture: 'beers'});
+    cy.visit('http://localhost:3000/');
   })
 
   it('should have a sample test', () => {
-    expect(true).to.equal(true)
+    expect(true).to.equal(true);
   })
 
   it('should visit the homepage at the base url', () => {
-    cy.url().should('eq', 'http://localhost:3000/')
+    cy.url().should('eq', 'http://localhost:3000/');
   })
 
   it('should display the site\'s name and tagline', () => {
@@ -18,6 +18,18 @@ describe('Homepage', () => {
     .contains('salts and malts')
     .get('.tagline')
     .contains('a beer-food pairing app that rises in the yeast and sets in the waist')
+  })
+
+  it('should display cards on a grid that contain beer details', () => {
+    cy.get('.homepage-grid')
+    .get('.homepage-grid > :nth-child(1)')
+    .get('.homepage-grid > :nth-child(1) > :nth-child(1)')
+    .contains('Buzz')
+    .get('.homepage-grid > :nth-child(1) > :nth-child(2)')
+    .contains('abv: 4.5')
+    .get('.homepage-grid > :nth-child(1) > :nth-child(3)')
+    .contains('ibu: 60')
+    
   })
 
 
