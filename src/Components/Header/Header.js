70c8c6cc620a23ce3c8378ffe815fee51fa1fe3b
Regan Losey <regan.losey@gmail.com>
2022-01-17 09:50:21 -0700

    Adjust header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,10 +3,9 @@ import {Link, NavLink} from 'react-router-dom'
 import './Header.scss'
 
 const Header = () => {
-
   return (
     <header className="header">
-      <Link to={'/'} className="site-name">salts and malts</Link>
+      <Link to={'/'} className="site-name" style={{textDecoration: "none"}}>salts and malts</Link>
       <p className="tagline">a beer-food pairing app that rises in the yeast and sets in the waist</p>
     </header>
   )
