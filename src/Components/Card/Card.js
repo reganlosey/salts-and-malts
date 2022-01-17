79ee5c526934df45c3b198be2c1d937adda635a9
Regan Losey <regan.losey@gmail.com>
2022-01-16 18:58:52 -0700

    Add beer icon

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './Card.scss'
+// import '../../../public/pint-of-beer.svg'
+
 
 
 const Card = ({ id, name, abv, ibu, tagline, description }) => {
@@ -9,6 +11,7 @@ const Card = ({ id, name, abv, ibu, tagline, description }) => {
       <p>{name}</p>
       <p>abv: {abv}</p>
       <p>ibu: {ibu}</p>
+      <img className="beer-icon" src="https://www.svgrepo.com/show/149438/pint-of-beer.svg" />
       <Link className="info-link" to={'/' + id} key={id} style={{ textDecoration: "none" }}>
         click here to see more</Link>
     </article>
