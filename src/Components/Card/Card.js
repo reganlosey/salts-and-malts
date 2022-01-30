9d28ce7760f42216b7a978953fc939cda58540fe
Regan Losey <regan.losey@gmail.com>
2022-01-29 23:45:01 -0700

    Add beer images to cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,19 +3,22 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import './Card.scss'
 
-const Card = ({ id, name, abv, ibu}) => {
+const Card = ({ id, name, img, abv, ibu }) => {
   return (
     <article className="beer-card" key={id}>
       <div className="text-container">
-      <p className="card-name">{name}</p>
-      <p>ABV: {abv}</p>
-      <p>IBU: {ibu}</p>
-      <Link
-        to={'/' + id}
-        key={id}
-        style={{ textDecoration: "none" }}>
-        <button className="about-btn">More about this brew</button>
-      </Link>
+        {/* <div className="img-container"> */}
+          <p className="card-name">{name}</p>
+          <img className="card-img" src={img} />
+        {/* </div> */}
+        <p>ABV: {abv}</p>
+        <p>IBU: {ibu}</p>
+        <Link
+          to={'/' + id}
+          key={id}
+          style={{ textDecoration: "none" }}>
+          <button className="about-btn">More about this brew</button>
+        </Link>
       </div>
     </article>
   )
